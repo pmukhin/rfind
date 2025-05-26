36405740a0de409a9c12926220160529160f6bd4
pmukhin <pauri.lentonen@gmail.com>
2025-05-26 09:49:01 +0200

    basic impl

---
+++ src/config.rs
@@ -0,0 +1,156 @@
+use std::path::{Path, PathBuf};
+use clap::Parser;
+use regex::Regex;
+use std::process::exit;
+
+#[derive(PartialEq, Debug)]
+pub enum FindType {
+    File,
+    Dir,
+    Symlink,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum SizeType {
+    Eq(u64),
+    Gte(u64),
+    Le(u64),
+}
+
+#[derive(Debug)]
+enum SizeTypeParseError {
+    NonDecodeableSize,
+}
+
+impl SizeType {
+    fn parse(s: &str) -> Result<Self, SizeTypeParseError> {
+        let re = Regex::new(r"^([+-]?)([0-9]+)([KMG]?)$").unwrap();
+        if let Some(caps) = re.captures(s) {
+            let multiplier = match caps.get(3).unwrap().as_str() {
+                "K" => 1024,
+                "M" => 1024 * 1024,
+                "G" => 1024 * 1024 * 1024,
+                "" => 1,
+                _ => return Err(SizeTypeParseError::NonDecodeableSize),
+            };
+            let value_in_bytes = caps.get(2).unwrap().as_str().parse().unwrap();
+            match caps.get(1).unwrap().as_str() {
+                "+" => Ok(SizeType::Gte(value_in_bytes * multiplier)),
+                "-" => Ok(SizeType::Le(value_in_bytes * multiplier)),
+                _ => Ok(SizeType::Eq(value_in_bytes)),
+            }
+        } else {
+            Err(SizeTypeParseError::NonDecodeableSize)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub dir: PathBuf,
+    pub find_type: FindType,
+    pub size_in_bytes: Option<SizeType>,
+    pub regex: Option<Regex>,
+    pub depth: Option<u16>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            dir: Path::new(".").to_owned(),
+            find_type: FindType::File,
+            size_in_bytes: None,
+            regex: None,
+            depth: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum NameMatcherError {
+    MoreThanOneMatcher,
+}
+
+fn create_name_matcher(
+    regex: Option<String>,
+    name: Option<String>,
+    iname: Option<String>,
+) -> Result<Regex, NameMatcherError> {
+    match (regex, name, iname) {
+        (Some(regex), None, None) => Ok(Regex::new(&regex).unwrap()),
+        (None, Some(name), None) => {
+            let fixed_name = name.replace("*", "(.*)");
+            Ok(Regex::new(&format!("^{}$", fixed_name)).unwrap())
+        }
+        (None, None, Some(iname)) => {
+            let fixed_name = iname.replace("*", "(.*)");
+            Ok(Regex::new(&format!("(?i)^{}$", fixed_name)).unwrap())
+        }
+        (None, None, None) => unreachable!(),
+        _ => Err(NameMatcherError::MoreThanOneMatcher),
+    }
+}
+
+impl Config {
+    pub fn parse() -> Config {
+        let raw_config = RawConfig::parse();
+        let mut config = Config::default();
+        config.dir = Path::new(&raw_config.dir).to_owned();
+
+        if raw_config.find_type == "d" {
+            config.find_type = FindType::Dir;
+        } else if raw_config.find_type == "s" {
+            config.find_type = FindType::Symlink;
+        } else if raw_config.find_type != "f" {
+            eprintln!("unknown type: {}", raw_config.find_type);
+            exit(1)
+        }
+
+        if let Some(size) = raw_config.size {
+            let st = SizeType::parse(&size);
+            if st.is_err() {
+                eprintln!("unknown size type: {}", size);
+                exit(1);
+            }
+            config.size_in_bytes = Some(st.unwrap());
+        }
+
+        if raw_config.regex.is_some() ||
+            raw_config.name.is_some() ||
+            raw_config.iname.is_some() {
+            let matcher_result =
+                create_name_matcher(raw_config.regex, raw_config.name, raw_config.iname);
+            if matcher_result.is_err() {
+                eprintln!("name matcher can't be decoded: {:?}", matcher_result.unwrap_err());
+                exit(1);
+            }
+            config.regex = Some(matcher_result.unwrap());
+        }
+        if let Some(depth) = raw_config.depth {
+            if depth == 0 {
+                eprintln!("depth should be >0");
+                exit(1);
+            }
+            config.depth = Some(depth);
+        }
+
+        config
+    }
+}
+
+#[derive(Debug, Parser)]
+struct RawConfig {
+    dir: String,
+    #[arg(long = "type", default_value = "f")]
+    find_type: String,
+    #[arg(long, allow_hyphen_values = true)]
+    size: Option<String>,
+    #[arg(long)]
+    name: Option<String>,
+    #[arg(long)]
+    iname: Option<String>,
+    #[arg(long)]
+    regex: Option<String>,
+    #[arg(long)]
+    depth: Option<u16>,
+}
---
+++ src/main.rs
@@ -0,0 +1,160 @@
+#![feature(let_chains)]
+
+mod config;
+
+use crate::config::{FindType, SizeType};
+use config::Config;
+use std::fs;
+use std::path::Path;
+use std::process::exit;
+
+#[macro_export]
+macro_rules! print_error {
+    ($($arg:tt)*) => {
+        eprintln!("find: {}", format!($($arg)*))
+    };
+}
+
+#[derive(Eq, PartialEq, Debug)]
+enum PathType {
+    Directory,
+    File,
+    Symlink,
+    Unknown,
+}
+
+fn get_type(path: &Path) -> PathType {
+    match fs::symlink_metadata(path) {
+        Ok(meta) if meta.file_type().is_dir() => PathType::Directory,
+        Ok(meta) if meta.file_type().is_symlink() => PathType::Symlink,
+        Ok(meta) if meta.file_type().is_file() => PathType::File,
+        _ => PathType::Unknown,
+    }
+}
+
+struct Find {
+    config: Config,
+    depth: u16,
+}
+
+impl Find {
+    fn match_name(&self, path: &Path) -> bool {
+        match &self.config.regex {
+            None => true,
+            Some(matcher) => {
+                let maybe_file_name =
+                    path.file_name().and_then(|os_str| os_str.to_str());
+                match maybe_file_name {
+                    None => false,
+                    Some(file_name) => matcher.is_match(file_name),
+                }
+            }
+        }
+    }
+
+    fn match_size(&self, path: &Path) -> bool {
+        let size = match path.metadata() {
+            Ok(m) => m.len(),
+            Err(_) => return false,
+        };
+        match &self.config.size_in_bytes {
+            None => true,
+            Some(SizeType::Gte(s)) => size >= *s,
+            Some(SizeType::Eq(s)) => size == *s,
+            Some(SizeType::Le(s)) => size <= *s,
+        }
+    }
+
+
+    fn find_type_is_dir(&self) -> bool {
+        self.config.find_type == FindType::Dir
+    }
+
+    fn file_matches(&self, path: &Path) -> bool {
+        self.config.find_type == FindType::File
+            && self.match_name(&path)
+            && self.match_size(&path)
+    }
+
+    fn symlink_matches(&self, path: &Path) -> bool {
+        self.config.find_type == FindType::Symlink
+            && self.match_name(&path)
+    }
+
+    pub fn run(&mut self, path: &Path) {
+        match get_type(&path) {
+            PathType::File if self.file_matches(path) => println!("{}", path.display()),
+            PathType::Symlink if self.symlink_matches(path) => println!("{}", path.display()),
+            PathType::Directory => {
+                if self.find_type_is_dir() {
+                    println!("{}", path.display());
+                }
+                self.inspect_dir(path);
+            }
+            _ => {}
+        }
+    }
+
+    fn inspect_dir(&mut self, path: &Path) {
+        if let Some(depth) = self.config.depth && self.depth == depth {
+            return;
+        }
+
+        self.depth += 1;
+
+        match fs::read_dir(path) {
+            Err(e) => print_error!("{}", e),
+            Ok(entries) => {
+                for entry in entries {
+                    if let Ok(entry) = entry {
+                        self.run(&entry.path());
+                    } else {
+                        print_error!("{}: ", entry.err().unwrap());
+                    }
+                }
+            }
+        }
+
+        self.depth -= 1;
+    }
+}
+
+fn main() {
+    let config = Config::parse();
+
+    if !config.dir.exists() {
+        print_error!("{}: no such file or directory", config.dir.display());
+        exit(1);
+    }
+
+    let root_path = config.dir.clone();
+
+    let mut find = Find {
+        config,
+        depth: 0,
+    };
+
+    find.run(&root_path);
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use std::os::unix;
+    use crate::{get_type, PathType};
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_get_type() {
+        assert_eq!(get_type(&PathBuf::from("/foo")), PathType::Unknown);
+        assert_eq!(get_type(&PathBuf::from("./src/main.rs")), PathType::File);
+        assert_eq!(get_type(&PathBuf::from("./src")), PathType::Directory);
+
+        unix::fs::symlink(&PathBuf::from("./src/main.rs"),
+                          &PathBuf::from("./main.rs.sl")).unwrap();
+
+        assert_eq!(get_type(&PathBuf::from("./main.rs.sl")), PathType::Symlink);
+
+        fs::remove_file(&PathBuf::from("./main.rs.sl")).unwrap()
+    }
+}
